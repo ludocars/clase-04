2130a0b122e8a2ed25557ed0b2bc49425b6c8e74
Carlos Flores <carlos_4_92@hotmail.com>
2019-11-28 22:02:31 -0500

    se modifico el codigo fuente

--- MyHangManGame/MyHangManGame/Program.cs
+++ MyHangManGame/MyHangManGame/Program.cs
@@ -8,23 +8,57 @@ namespace MyHangManGame {
     class Program {
         static void Main (string[] args) {
             //asignacion de variables
-            string secretword = "zapato";
+            string secretword = "zapatos";
+            string publicword = "";
             string letter = "";
             int hp = 3;
-
-            //procesos
-            for (int i = 0; i < secretword.Length; i++) {
-                Console.Write ("*");
-            }
-            Console.WriteLine ();
-
-            // Console.WriteLine (secretword.Length); cuenta la cantidad de letras en la palabra secreta
-            Console.WriteLine ("ingrese la letra: \n");
-            letter = Console.ReadLine ();
+            string tempword = "";
             
-            // Console.WriteLine("> \t"+letter) muestra la letra que se ingreso
+            //PROCESOS:
 
-            Console.ReadLine(); //evita el cierre del programa
+            for (int i = 0; i < secretword.Length; i++) {
+                tempword += "*"; 
+            }
+            publicword = tempword;
+            Console.WriteLine (publicword);
+
+            while (hp > 0) {
+                Console.WriteLine ("HP = " + hp);
+                Console.WriteLine ("ingrese una letra: ");
+                letter = Console.ReadLine();
+                Console.Clear ();
+
+                tempword = "";
+                for (int i=0; i < secretword.Length; i++) {
+                    if (letter == "" + secretword[i]) {
+                        tempword += letter;
+                    }
+                    else {
+                        tempword += publicword[i];
+                    }
+                }
+                if (publicword == tempword) {
+                    hp--;
+                }
+                if (secretword == tempword) {
+                    Console.WriteLine ("YOU WIN!");
+                    break;
+                }
+                publicword = tempword;
+                Console.WriteLine (publicword);
+            }
+            if (hp <= 0) {
+                Console.WriteLine ("GAME OVER");
+            }
+            Console.ReadLine();
         }
     }
 }
+    //foreach (char c in secretword) {
+                //    if (letter == "" + c) {
+                //        tempword += c; //tempword = temoword + c; suma a tempword el caracter "c"
+                //  } else {
+                //        tempword += "*";
+                //    }
+     // Console.WriteLine (secretword.Length); cuenta la cantidad de letras en la palabra secreta
+       // Console.WriteLine("> \t"+letter) muestra la letra que se ingreso
