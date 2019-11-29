4f350b34fa1fd7af4566060891b20e4a1dbdb949
Carlos Flores <carlos_4_92@hotmail.com>
2019-11-28 20:27:41 -0500

    se genero el codigo de programacion en visual

--- MyHangManGame/MyHangManGame/Program.cs
+++ MyHangManGame/MyHangManGame/Program.cs
@@ -7,6 +7,24 @@ using System.Threading.Tasks;
 namespace MyHangManGame {
     class Program {
         static void Main (string[] args) {
+            //asignacion de variables
+            string secretword = "zapato";
+            string letter = "";
+            int hp = 3;
+
+            //procesos
+            for (int i = 0; i < secretword.Length; i++) {
+                Console.Write ("*");
+            }
+            Console.WriteLine ();
+
+            // Console.WriteLine (secretword.Length); cuenta la cantidad de letras en la palabra secreta
+            Console.WriteLine ("ingrese la letra: \n");
+            letter = Console.ReadLine ();
+            
+            // Console.WriteLine("> \t"+letter) muestra la letra que se ingreso
+
+            Console.ReadLine(); //evita el cierre del programa
         }
     }
 }
